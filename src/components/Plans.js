b121cb4488b905125e144ad28d4d11d3c60ec474
Daniel Ohinoyi <bizwithdan@gmail.com>
2024-03-21 11:46:19 +0100

    Bug Fix

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -11,6 +11,7 @@ const Plans = () => {
 
     const user = useSelector(selectUser);
 
+    // While using Static data to represent subscription 
     const data = {
         basic: {
             name: "Basic Plan",
@@ -29,102 +30,148 @@ const Plans = () => {
         }
     }
 
-    const loadCheckout = async (priceId) => {
-        const docRef = await db
-            .collection('customers')
-            .doc(user.uid)
-            .collection("checkout_sessions")
-            .add({
-                price: priceId,
-                success_url: window.location.origin,
-                cancel_url: window.location.origin
-            });
-
-        docRef.onSnapshot(async (snap) => {
-            const { error, sessionId } = snap.data();
-
-            if (error) {
-                alert(`An error occured: ${error.message}`)
-            } else if (sessionId) {
-                const stripe = await loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY);
-                stripe.redirectToCheckout({ sessionId })
-            }
-        })
-    }
-
     useEffect(() => {
-        db.collection('customers')
-            .doc(user.uid)
-            .collection('subscriptions')
-            .get()
-            .then(querySnapshot => {
-                querySnapshot.forEach(async subscription => {
-                    setSubscription({
-                        role: subscription.data().role,
-                        current_period_end: subscription.data().current_period_end.seconds,
-                        current_period_start: subscription.data().current_period_start.seconds,
-                    })
-                })
-            })
-    }, [user.uid])
-
-    useEffect(() => {
-        db.collection('products')
-            .where('active', '===', true)
-            .get().then(querySnapshot => {
-                const products = {};
-                querySnapshot.forEach(async productDoc => {
-                    products[productDoc.id] = productDoc.data();
-                    const priceSnap = await productDoc.ref.collection('prices').get();
-                    priceSnap.docs.forEach(price => {
-                        products[productDoc.id].prices = {
-                            priceId: price.id,
-                            priceData: price.data()
-                        }
-                    })
-                });
-                setProducts(products);
-            });
-    }, []);
+        console.log(data)
+    })
 
 
+    // While using Stripe Mechanism to collecy subscription payment
+
+    // const loadCheckout = async (priceId) => {
+    //     const docRef = await db
+    //         .collection('customers')
+    //         .doc(user.uid)
+    //         .collection("checkout_sessions")
+    //         .add({
+    //             price: priceId,
+    //             success_url: window.location.origin,
+    //             cancel_url: window.location.origin
+    //         });
+
+    //     docRef.onSnapshot(async (snap) => {
+    //         const { error, sessionId } = snap.data();
+
+    //         if (error) {
+    //             alert(`An error occured: ${error.message}`)
+    //         } else if (sessionId) {
+    //             const stripe = await loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY);
+    //             stripe.redirectToCheckout({ sessionId })
+    //         }
+    //     })
+    // }
+
+    // useEffect(() => {
+    //     db.collection('customers')
+    //         .doc(user.uid)
+    //         .collection('subscriptions')
+    //         .get()
+    //         .then(querySnapshot => {
+    //             querySnapshot.forEach(async subscription => {
+    //                 setSubscription({
+    //                     role: subscription.data().role,
+    //                     current_period_end: subscription.data().current_period_end.seconds,
+    //                     current_period_start: subscription.data().current_period_start.seconds,
+    //                 })
+    //             })
+    //         })
+    // }, [user.uid])
+
+    // useEffect(() => {
+    //     db.collection('products')
+    //         .where('active', '===', true)
+    //         .get().then(querySnapshot => {
+    //             const products = {};
+    //             querySnapshot.forEach(async productDoc => {
+    //                 products[productDoc.id] = productDoc.data();
+    //                 const priceSnap = await productDoc.ref.collection('prices').get();
+    //                 priceSnap.docs.forEach(price => {
+    //                     products[productDoc.id].prices = {
+    //                         priceId: price.id,
+    //                         priceData: price.data()
+    //                     }
+    //                 })
+    //             });
+    //             setProducts(products);
+    //         });
+    // }, []);
+
+
+
+    // return (
+    //     <div className=''>
+    //         <br />
+    //         {subscription && <p className=''>
+    //             Renewal Date {
+    //                 new Date(subscription?.current_period_end * 1000)
+    //                     .toLocaleDateString()
+    //             }
+    //         </p>}
+
+    //         {Object.entries(products).map(([productId, productData]) => {
+    //             // Logic to check if the user's subscription is active
+
+    //             const isCurrentPackage = productData?.name?.toLowerCase()
+    //                 .includes(subscription?.role)
+
+    //             return (
+    //                 <div
+    //                     key={productId}
+    //                     className={`flex justify-between p-5 opacity-80 hover:opacity-100`}
+    //                 >
+    //                     <div className=''>
+    //                         <h5 className='text-[13px] font-bold'>
+    //                             {productData.name}
+    //                         </h5>
+    //                         <h6 className='text-[10.72px] font-bold'>
+    //                             {productData.description}
+    //                         </h6>
+    //                     </div>
+    //                     <button
+    //                         className='py-2.5 px-5 text-base bg-netflix 
+    //                         font-semibold border-none cursor-pointer' className={`py-2.5 px-5 text-base bg-netflix font-semibold border-none
+    //                          cursor-pointer ${isCurrentPackage && '!bg-gray-500'}`}
+    //                         onClick={() => !isCurrentPackage && loadCheckout(productData?.prices?.priceId)}
+    //                     >
+    //                         {isCurrentPackage ? "Current Package" : "Subscribe"}
+    //                     </button>
+    //                 </div>
+    //             )
+    //         })}
+
+    //     </div>
+    // )
 
     return (
         <div className=''>
             <br />
             {subscription && <p className=''>
-                Renewal Date {
-                    new Date(subscription?.current_period_end * 1000)
-                        .toLocaleDateString()
+                Renewal Date: {
+                    new Date(Date.now() + (1000 * 60 * 60 * 24 * 30)).toLocaleDateString()
                 }
             </p>}
 
-            {Object.entries(products).map(([productId, productData]) => {
+            {Object.entries(data).map(([productId, productData]) => {
                 // Logic to check if the user's subscription is active
 
-                const isCurrentPackage = productData?.name?.toLowerCase()
-                    .includes(subscription?.role)
+                const isCurrentPackage = productData?.name?.includes(subscription?.productData?.name)
 
                 return (
                     <div
                         key={productId}
-                        className={`
-                            flex justify-between p-5 opacity-80 hover:opacity-100
-                            ${isCurrentPackage && '!bg-gray-500'}
-                        `}
+                        className={`flex justify-between p-5 opacity-80 hover:opacity-100`}
                     >
                         <div className=''>
                             <h5 className='text-[13px] font-bold'>
                                 {productData.name}
                             </h5>
                             <h6 className='text-[10.72px] font-bold'>
-                                {productData.description}
+                                {productData.description}  &nbsp; (${productData.price})
                             </h6>
                         </div>
                         <button
-                            className='py-2.5 px-5 text-base bg-netflix 
-                            font-semibold border-none cursor-pointer'
-                            onClick={() => !isCurrentPackage && loadCheckout(productData?.prices?.priceId)}
+                            className={`py-2.5 px-5 text-base bg-netflix font-semibold border-none
+                             cursor-pointer ${isCurrentPackage && '!bg-gray-500'}`}
+                            onClick={() => !isCurrentPackage && setSubscription({ productData })}
                         >
                             {isCurrentPackage ? "Current Package" : "Subscribe"}
                         </button>
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -19,12 +19,12 @@ const ProfilePage = () => {
     return (
         <div className='h-screen'>
             <Nav />
-            <div className='flex flex-col w-1/2 mx-auto pt-[8%] max-w-[800px]'>
+            <div className='flex flex-col w-1/2 mx-auto lg:pt-[8%] pt-[10%] mb-[200px] max-w-[800px]'>
                 <h1 className='text-6xl font-normal border-b border-[#282c2d] mb-5'>
                     Edit Profile
                 </h1>
 
-                <div className='flex'>
+                <div className='flex mb-10'>
                     <img
                         src={process.env.PUBLIC_URL + "/images/user-avatar.jpg"}
                         alt=''
@@ -35,7 +35,7 @@ const ProfilePage = () => {
                             {user?.email || "bizwithdan@gmail.com"}
                         </h2>
                         <div className='mt-5'>
-                            <h3 className='border-b border-[#282c2d] pb-2.5'>Plans</h3>
+                            <h3 className='border-b border-[#282c2d] pb-2.5 text-bold text-lg'>PLANS</h3>
                             <Plans />
                             <button
                                 className='w-full py-3 px-5 mt-[5%] text-base border-none hover:brightness-75 
